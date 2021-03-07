4c10ab13bd3f14996cc628997c4767c6e279251d
Kacpereterefere <kacpergawel1@wp.pl>
2021-03-07 11:08:30 +0100

    Delete from Cart

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
-import {Button, Col, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
+import {Button, Card, Col, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import Login from "../pages/LoginScreen";
 import {useMediaQuery} from "react-responsive/src";
-import {useSelector} from "react-redux";
-import {useState} from "react";
-import ErrorBox from "./Error";
+import {useDispatch, useSelector} from "react-redux";
+import React, {useEffect, useState} from "react";
+import {removeFromCart} from "../actions/cartActions";
+import {Link} from "react-router-dom";
 
 
 export default function MyNavbar() {
@@ -13,14 +14,17 @@ export default function MyNavbar() {
     const isSmallMedia =  useMediaQuery({ query: '(max-width: 1028px)' });
     const cart = useSelector((state) => state.cart);
     const {cartItems} = cart;
-
+    const dispatch = useDispatch();
     const [show, setShow] = useState(false);
+
+
     const showDropdown = (e)=>{
         setShow(!show);
     }
     const hideDropdown = e => {
         setShow(false);
     }
+
 
     return (
         <Navbar  expand="lg" bg="dark" variant="dark">
@@ -32,34 +36,40 @@ export default function MyNavbar() {
                 </Nav>
                 <Nav className="mr-lg-5">
                     {
-                        isSmallMedia ? <Nav.Link href="/cart"><div style={{display: "inline-block"}}><FontAwesomeIcon icon={faShoppingCart} /> Koszyk(0) </div></Nav.Link> : (
-                            <NavDropdown show={show}  onMouseEnter={showDropdown} onMouseLeave={hideDropdown} className="mr-lg-5" title={<div style={{display: "inline-block"}}><FontAwesomeIcon icon={faShoppingCart} /> Koszyk(0) </div>} id="collasible-nav-dropdown">
+                        isSmallMedia ? <Nav.Link href="/cart"><div style={{display: "inline-block"}}><FontAwesomeIcon icon={faShoppingCart} /> Koszyk({cartItems.length}) </div></Nav.Link> : (
+                            <NavDropdown show={show}  onMouseEnter={showDropdown} onMouseLeave={hideDropdown} className="mr-lg-5" title={<div style={{display: "inline-block"}}><FontAwesomeIcon icon={faShoppingCart} /> Koszyk({cartItems.length}) </div>} id="collasible-nav-dropdown">
 
                                 {
                                     cartItems.length === 0 ? (<h6 className="text-danger ml-1 p-2">Koszyk jest pusty!</h6>) : (
 
                                     cartItems.map((item) => (
                                         <>
-                                            <NavDropdown.Item href={`/product/${item.product}`} key={item._id}>
+                                            <NavDropdown.ItemText  role="button" key={item.product}>
                                                 <Row className="mr-auto">
                                                     <Col lg="4">
-                                                        <img style={{marginRight: "20px"}} src={item.image} alt="" width="100px" height="100px"/>
+                                                        <a href={`/product/${item.product}`}>
+                                                            <img className="cartImg" style={{marginRight: "20px"}} src={item.image} alt={`cartItem${item.product}`} width="100px" height="100px"/>
+                                                        </a>
                                                     </Col>
                                                     <Col lg="4">
-                                                        <p className="mt-4" style={{marginRight: "200px",whiteSpace: "nowrap", overflow: "hidden",textOverflow: "ellipsis",width: "13ch"}}>
-                                                            {item.name}
-                                                        </p>
+                                                        <a className="link-color-carousel" href={`/product/${item.product}`}>
+                                                            <p className="mt-4" style={{marginRight: "200px",whiteSpace: "nowrap", overflow: "hidden",textOverflow: "ellipsis",width: "13ch"}}>
+                                                                {item.name}
+                                                            </p>
+                                                        </a>
                                                     </Col>
                                                     <Col lg="4">
-                                                        <Button variant="outline-danger" className="mt-4">X</Button>
+                                                        <Button role="button" variant="outline-danger" onClick={() => dispatch(removeFromCart(item.product))} className="mt-4">X</Button>
                                                     </Col>
                                                 </Row>
-                                            </NavDropdown.Item>
+                                            </NavDropdown.ItemText>
                                             <NavDropdown.Divider />
-                                            <NavDropdown.Item href="/cart">Przejdź do koszyka</NavDropdown.Item>
                                         </>
 
                                     )))
+                                }
+                                {
+                                    cartItems.length > 0 && <NavDropdown.Item href="/cart">Przejdź do koszyka</NavDropdown.Item>
                                 }
 
                             </NavDropdown>
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,8 @@
-import {CART_ADD_ITEM} from "../constants/cartConstants";
+import {CART_ADD_ITEM, CART_REMOVE_ITEM} from "../constants/cartConstants";
 
 export const cartReducer = (state = {cartItems: []}, action) => {
     switch (action.type){
+
         case CART_ADD_ITEM:
             const item = action.payload;
             const existingItem = state.cartItems.find(x => x.product === item.product);
@@ -15,6 +16,10 @@ export const cartReducer = (state = {cartItems: []}, action) => {
                     ...state, cartItems: [...state.cartItems, item]
                 };
             }
+
+        case CART_REMOVE_ITEM:
+            return {...state, cartItems: state.cartItems.filter(x => x.product !== action.payload),}
+
         default:
             return state;
     }
