59b1c2c63b35872c680341b0a1082193d7c21892
Kacpereterefere <kacpergawel1@wp.pl>
2021-03-07 20:02:14 +0100

    Conecting to MongoDB Database

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -9,3 +9,24 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
     const createdUsers = await User.insertMany(data.users);
     res.send({createdUsers});
 }));
+
+userRouter.post('/signin', expressAsyncHandler(async (req, res) =>{
+    const user = await User.findOne({email: req.body.email});
+    if(user){
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user),
+            });
+            return;
+        }else {
+            res.status(401).send({message: 'Niepoprawny email lub hasło'});
+        }
+    }else {
+        res.status(401).send({message: 'Niepoprawny email lub hasło'});
+    }
+
+}));
