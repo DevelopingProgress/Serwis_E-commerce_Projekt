c97a451dcb174b3e3b966d2568c47d77ead593dc
Kacper <kacpergawel1@wp.pl>
2021-04-05 22:02:59 +0200

    Finishing Project Stage

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,22 @@ app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+});
 
 app.get('/api/config/paypal', (req, res) => {
     const paypalId = process.env.PAYPAL_CLIENT || 'sb';
     res.send(paypalId);
 });
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
