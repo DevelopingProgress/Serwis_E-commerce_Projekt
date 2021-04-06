b5c3cb88424426f2b03e0b8dfcb9e84940c7fe54
Kacper <kacpergawel1@wp.pl>
2021-04-06 12:47:53 +0200

    Heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-
+app.use(rn);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
