200bc2db662f011e410eb5f070c9bdd77c859881
Kacper <kacpergawel1@wp.pl>
2021-05-15 09:57:59 +0200

    Finishing Project Stage

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,20 +1,19 @@
 import multer from 'multer';
-import express from "express";
-import {isAuth} from "../util.js";
+import express from 'express';
+import { isAuth } from '../util.js';
 
 const uploadRouter = express.Router();
 
-
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, 'uploads/');
     },
     filename(req, file, cb) {
         cb(null, `${Date.now()}.jpg`);
-    }
+    },
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
     res.send(`/${req.file.path}`);
